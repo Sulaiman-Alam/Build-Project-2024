b7819539fdba844c374be4e82ebd4f25d2bb7235
Sulaiman-Alam <108904299+Sulaiman-Alam@users.noreply.github.com>
2024-10-13 17:39:12 -0400

    Add files via upload

---
+++ Final Project/wwwroot/PageModels/TeamModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BuildProjectSummer2024.Models.PageModels
+{
+    public class TeamModel
+    {
+        public int Id { get; set; }
+
+        public string TeamName { get; set; } = null!;
+
+        public string? City { get; set; }
+
+        public int StadiumId { get; set; }
+        public int PlayerCount { get; set; }
+
+        public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+
+        public virtual Stadium Stadium { get; set; } = null!;
+    }
+}
---
+++ Final Project/wwwroot/PlayerRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildProjectSummer2024.Models;
+
+public partial class PlayerRole
+{
+    public int Id { get; set; }
+
+    public string? RoleName { get; set; }
+
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+}
